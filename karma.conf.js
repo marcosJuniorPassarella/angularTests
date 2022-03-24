36fa938d281c03517de37d664e9a114440c114fd
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-03-24 16:11:21 -0300

    master - Added coverage tests and reporter

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,9 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -30,6 +32,12 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+     FirefoxSemCabeca: {
+       base: 'Firefox',
+       flags: ['-headless']
+     } 
+    }
   });
 };
