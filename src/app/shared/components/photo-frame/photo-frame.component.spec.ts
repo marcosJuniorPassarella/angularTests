9b62a4de72f7fc774b41c49eb3cf740e95cccd03
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-03-25 18:38:51 -0300

    master - Added ui tests

---
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -0,0 +1,84 @@
+import { PhotoFrameComponent } from './photo-frame.component';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { PhotoFrameModule } from './photo-frame.module';
+
+describe(PhotoFrameComponent.name, () => {
+  let fixture: ComponentFixture<PhotoFrameComponent> = null;
+  let component: PhotoFrameComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PhotoFrameModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PhotoFrameComponent);
+    component = fixture.componentInstance;
+  });
+
+  // TESTA SE O COMPONENTE FOI CRIADO
+  it('Should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  // TESTA SE O MÉTODO LIKE DEVE DISPARAR A OUTPUT LIKED UMA ÚNICA VEZ
+  // QUANDO CHAMADOS MÚLTIPLAS VEZES DENTRO DO TEMPO DE DEBOUNCE
+  it(`#${PhotoFrameComponent.prototype.like.name}
+  should trigger (@Output liked) once when called multiple times within
+  debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => times++);
+    component.like();
+    component.like();
+    tick(500);
+    expect(times).toBe(1);
+  }));
+
+  // like incremente duas vezes se tiver dentro do tempo do debounce
+  it(`#${PhotoFrameComponent.prototype.like.name}
+    should trigger (@Output liked) two times when called outside debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => times++);
+    component.like();
+    tick(500);
+    component.like();
+    tick(500);
+    expect(times).toBe(2);
+  }));
+
+  // TESTA SE ESTÁ SENDO EXIBIDO O NÚMERO DE LIKES COMPUTADOS AO CLICAR NO LIKE
+  it(`Should display number of likes when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    // ACESSANDO NOSSO ELEMENTO DO DOM QUE DIZ A QUANTIDADE DE LIKES
+    const element: HTMLElement =
+      fixture.nativeElement.querySelector('.like-counter');
+    expect(element.textContent.trim()).toBe('1');
+  });
+
+  // TESTA SE AO INCREMENTAR NOSSO NÚMERO DE LIKES O ARIA-LABEL EXIBE CORRRETAMENTE
+  // O NÚMERO DE LIKES
+  it(`(D) Should update aria-label when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('1: people liked');
+  });
+
+  // TESTA SE AO NÃO CLICAR NO BOTÃO DE LIKE O CONTEÚDO DO ARIA-LABEL É O QUE
+  // ESPERAMOS QUE SEJA
+  it(`(D) Should have aria-label with 0 (@Input likes) value`, () => {
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('0: people liked');
+  });
+
+});
--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,17 +1,43 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss'],
 })
-export class PhotoFrameComponent {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
   @Output() public liked: EventEmitter<void> = new EventEmitter();
   @Input() public description = '';
   @Input() public src = '';
   @Input() public likes = 0;
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubscribe: Subject<void> = new Subject();
+
+  public ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      .pipe(debounceTime(500))
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => {
+        this.liked.emit();
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
 
   public like(): void {
-    this.liked.emit();
+    this.debounceSubject.next();
   }
 }
