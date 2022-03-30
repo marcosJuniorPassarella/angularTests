b6458b56d291d81a6026e6633a88f945c9a727af
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-03-30 08:56:39 -0300

    master - Created new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotoListModule } from './components/photo-list/photo-list.module';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
